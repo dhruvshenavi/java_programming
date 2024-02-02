6124c3b50d90a812e53479e04c3c61789489dc11
Dhruv Shenavi <dhruvshenavi@gmail.com>
2024-02-02 14:22:28 +0530

    completed program datatypepremitives.java

--- datatypeprimitives.java
+++ datatypeprimitives.java
@@ -8,6 +8,32 @@ public class datatypeprimitives {
 
         System.out.println("FLOAT MAX VALU "+Float.MAX_VALUE);
         System.out.println("FLOAT MIN VALU "+Float.MIN_VALUE);
-        System.out.println("FLOAT SIZE BYTES : "+Float.MAX_VALUE);
+        System.out.println("FLOAT SIZE BYTES : "+Float.SIZE+'\n');
+
+        System.out.println("INTEGER MAX VALU "+Integer.MAX_VALUE);
+        System.out.println("INTEGER MIN VALU "+Integer.MIN_VALUE);
+        System.out.println("INTEGER SIZE BYTES : "+Integer.SIZE+'\n');
+
+        System.out.println("charater MAX VALU "+Character.MAX_VALUE);
+        System.out.println("charater MIN VALU "+Character.MIN_VALUE);
+        System.out.println("charter SIZE BYTES : "+Character.SIZE+'\n');
+
+        System.out.println("Double MAX VALU "+Double.MAX_VALUE);
+        System.out.println("Double MIN VALU "+Double.MIN_VALUE);
+        System.out.println("Double SIZE BYTES : "+Double.SIZE+'\n');
+
+
+        System.out.println("LONG MAX VALU "+Long.MAX_VALUE);
+        System.out.println("LONG MIN VALU "+Long.MIN_VALUE);
+        System.out.println("LONG SIZE BYTES : "+Long.SIZE+'\n');
+
+        System.out.println("Short MAX VALU "+Short.MAX_VALUE);
+        System.out.println("short MIN VALU "+Short.MIN_VALUE);
+        System.out.println("short SIZE BYTES : "+Short.SIZE+'\n');
+
+        System.out.println("Byte MAX VALU "+Byte.MAX_VALUE);
+        System.out.println("Byte MIN VALU "+Byte.MIN_VALUE);
+        System.out.println("Byte SIZE BYTES : "+Byte.SIZE);
+
     }
 }
