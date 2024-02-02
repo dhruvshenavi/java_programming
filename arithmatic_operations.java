5254b321ac7de4a5cca9b0845ac42f0229d74d24
Dhruv Shenavi <dhruvshenavi@gmail.com>
2024-02-02 14:48:57 +0530

    understand concept of coersion...

---
+++ arithmatic_operations.java
@@ -0,0 +1,27 @@
+public class arithmatic_operations {
+    
+    /*understanding coersion : 
+     * 
+     * i+c=i
+     * i+f=f
+     * l+f=f
+     * i+l=f
+     * i+s=i
+     * s+c=i
+     * d+f=d
+     * d+l=f
+     * 
+     */
+
+    int i = 10;
+    char c = 2;
+    double d = 4;
+    float f = 5.6f;
+    long l = 78;
+    short s = 4;
+
+    int res1 = c + s;
+    double res2 = d + f;
+    float res3 = f + l;
+
+}
