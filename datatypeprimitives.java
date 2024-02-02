37c3a87e2dc5084ac5f1566ee3b6c53ac789790b
Dhruv Shenavi <dhruvshenavi@gmail.com>
2024-02-02 11:25:34 +0530

    Program to understand primitve data types ther upper bound and lower bound and using their class

---
+++ datatypeprimitives.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+/**
+ * datatypeprimitives
+ */
+public class datatypeprimitives {
+
+    public static void main(String[] args){
+
+        System.out.println("FLOAT MAX VALU "+Float.MAX_VALUE);
+        System.out.println("FLOAT MIN VALU "+Float.MIN_VALUE);
+        System.out.println("FLOAT SIZE BYTES : "+Float.MAX_VALUE);
+    }
+}
